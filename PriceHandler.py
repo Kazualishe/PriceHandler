82c04404f1236c42791ce219f7133d7fb420b6a9
Kazualishe <39470942+Kazualishe@users.noreply.github.com>
2023-11-03 14:59:25 +0300

    add RestApi module prototype

--- PriceHandler.py
+++ PriceHandler.py
@@ -1,6 +1,11 @@
-import sys
+#interface_modul_imports
 from PyQt5.QtWidgets import QWidget
+
+#rest_api_modul_imports
 import requests
+import json
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
 #data_model
 
@@ -73,4 +78,35 @@ class MainWindow(QWidget):
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('PriceHandler')
         self.show()
+        
+#rest_api
+class RestAPI_dns:
+    
+    def get_products(self, search: str) -> list:
+        headers = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'
+        }
+        url = f'https://www.dns-shop.ru/search/?q={search}&p=1&order=popular&stock=all'
+        session = requests.session()
+        session.headers.update(headers)
+
+        rs = session.get(url)
+        data = json.loads(rs.text)
+
+        root = BeautifulSoup(data['html'], 'html.parser')
+
+        items = []
+
+        for a in root.select('.product-info__title-link > a'):
+            items.append(
+                (a.get_text(strip=True), urljoin(rs.url, a['href']))
+            )
+
+        return items
+    
+    def test_search(self, name):
+        self.get_products()
+        for title, url in items:
+            print(f'    {title!r}: {url}')
         
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from PriceHandler import MainWindow
+from PriceHandler import MainWindow, RestAPI_dns
+
+def start_interface():
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
+    
+def test_rest_dns():
+    pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
